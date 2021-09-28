bdfcee439a3126b5789769a18c680f657b66061e
Matias Garcia <matiasigarcia91@gmail.com>
2021-09-28 11:18:47 +0200

    Introduce state

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 const ProductCard = props => {
-  console.log(props.name);
+  const [likes, setLikes] = useState(0);
+  const [bought, setBought] = useState(false);
+
+  const onLikeClick = () => {
+    setLikes(likes + 1);
+  };
+
   return (
     <li style={{ border: "2px solid white", margin: 10 }}>
       <h3>{props.name}</h3>
       <p>$ {props.price}</p>
+      <h4>Nr of likes: {likes}</h4>
+      <button onClick={onLikeClick}>Like!</button>
     </li>
   );
 };
--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -1,21 +1,35 @@
 import ProductCard from "./ProductCard";
+import { useState } from "react";
 
 const ProductList = () => {
-  const products = [
+  //actualValue of state = false   Create a state => boolean (false => initalState)
+  const [hidden, setHidden] = useState(false);
+
+  const [products, setProducts] = useState([
     { id: 1, name: "car", price: 13000 },
     { id: 2, name: "laptop", price: 1000 },
     { id: 3, name: "beers", price: 10 },
     { id: 4, name: "records", price: 130 },
-  ];
+  ]);
+
+  const onHideClick = () => {
+    console.log("I'm clicking the button!");
+    setHidden(!hidden);
+  };
 
   return (
     <div>
       <h1>Product List!</h1>
-      <ul>
-        {products.map((prod, index) => (
-          <ProductCard key={prod.id} name={prod.name} price={prod.price} />
-        ))}
-      </ul>
+      <button onClick={onHideClick}>
+        {hidden ? "Show List" : "Hide List"}
+      </button>
+      <div style={{ display: hidden ? "none" : "flex" }}>
+        <ul>
+          {products.map((prod, index) => (
+            <ProductCard key={prod.id} name={prod.name} price={prod.price} />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
